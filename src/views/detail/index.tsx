8b7ca0db9ea4db0920c783c087522476e2eb070c
JiuYue <www.echo7@qq.com>
2023-05-13 14:50:08 +0800

    增加了商品详情的轮播图

--- src/views/detail/index.tsx
+++ src/views/detail/index.tsx
@@ -7,6 +7,7 @@ import ProInfoComponent from './components/ProInfoComponent';
 import ProComponent from '../home/components/ProComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ActionBar from './components/action/index';
+import BannerComponent from '../home/components/BannerComponent'
 import { observer, inject } from 'mobx-react'
 
 
@@ -35,6 +36,7 @@ const Detail: FC<IDetailProps> = (props: any) => {
     useEffect(() => {
         getDetailData(proid!).then(res => {
             console.log(res.data.data)
+            console.log(res.data.data.img3)
             setObj({
                 banners: res.data.data.banners[0].split(','),
                 proname: res.data.data.proname,
@@ -47,6 +49,34 @@ const Detail: FC<IDetailProps> = (props: any) => {
             })
         })
     }, [proid])
+
+    // 轮播图逻辑
+    const [bannerList, setBannerList] = useState([])
+
+    // 遍历 res 对象中的属性，将以 "img" 开头的属性值存入数组
+    // for (const key in res) {
+    //     if (key.startsWith("img")) {
+    //         imageUrls.push(res[key]);
+    //     }
+    // }
+    useEffect(() => {
+        getDetailData(proid!).then(res => {
+            var data = res.data.data
+            console.log('data', data)
+            const tempImageUrls: string[] = []; // 创建一个临时数组
+            for (const key in data) {
+                if (key.startsWith("img")) {
+                    tempImageUrls.push(data[key]);
+                }
+            }
+            setBannerList(tempImageUrls as never[]); // 使用类型断言来匹配参数类型
+        });
+    }, [proid]);
+
+
+
+
+    // 轮播图逻辑
 
     // 商品推荐逻辑
     const [proList, setProlist] = useState([])
@@ -90,6 +120,8 @@ const Detail: FC<IDetailProps> = (props: any) => {
             {/* 顶部显示组件 */}
             <HeaderComponent></HeaderComponent>
             <div className="content">
+                {/* 轮播图逻辑 */}
+                <BannerComponent list={bannerList}></BannerComponent>
                 {/* 商品的详情数据 */}
                 <ProInfoComponent
                     originprice={obj.originprice}
